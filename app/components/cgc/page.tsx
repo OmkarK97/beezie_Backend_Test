ee337e4082e20ae06308dc4a845a3518c3b8e633
OmkarK97 <omkar139797@gmail.com>
2024-09-05 02:51:36 +0530

    vercel fix

--- app/components/cgc/page.tsx
+++ app/components/cgc/page.tsx
@@ -75,7 +75,7 @@ export default function Grading2({ Cert, Card_Name, Game, Year, Language, Card_S
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
                 <h2 className="mb-3 text-xl font-semibold">Grade:-</h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">{Grade[0]}</p>
+                <p className="m-0 max-w-[30ch] text-sm opacity-50">{Grade}</p>
             </div>
 
             <div
